73d23668303f6e360a175ea204b33a0e6c870faa
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-08 22:47:51 +0200

    Implemented Add Astronaut

---
+++ SpaceStation/Core/Controller.cs
@@ -0,0 +1,63 @@
+﻿using SpaceStation.Core.Contracts;
+using SpaceStation.Models.Astronauts;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Core
+{
+    public class Controller : IController
+    {
+        private PlanetRepository planets;
+        private AstronautRepository astronauts;
+
+        public Controller()
+        {
+            planets = new PlanetRepository();
+            astronauts = new AstronautRepository();
+        }
+
+        public string AddAstronaut(string type, string astronautName)
+        {
+            IAstronaut astronaut;
+            switch (type)
+            {
+                case "Biologist":
+                    astronaut = new Biologist(astronautName);
+                    break;
+                case "Geodesist":
+                    astronaut = new Geodesist(astronautName);
+                    break;
+                case "Meteorologist":
+                    astronaut = new Meteorologist(astronautName);
+                    break;
+                default:
+                    throw new InvalidOperationException("Astronaut type doesn't exists!");
+            }
+
+            return $"Successfully added {type}: {astronautName}!";
+        }
+
+        public string AddPlanet(string planetName, params string[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExplorePlanet(string planetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string RetireAstronaut(string astronautName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
