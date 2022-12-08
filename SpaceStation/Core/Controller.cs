a1eeae520e6335b5e594146ddc8624b812f670f5
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-09 00:06:24 +0200

    C# OOP Retake Exam - 22 August 2021

--- SpaceStation/Core/Controller.cs
+++ SpaceStation/Core/Controller.cs
@@ -1,9 +1,12 @@
 ﻿using SpaceStation.Core.Contracts;
 using SpaceStation.Models.Astronauts;
 using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission;
+using SpaceStation.Models.Planets;
 using SpaceStation.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace SpaceStation.Core
@@ -42,12 +45,28 @@ namespace SpaceStation.Core
 
         public string AddPlanet(string planetName, params string[] items)
         {
-            throw new NotImplementedException();
+            var planet = new Planet(planetName);
+
+            foreach (var item in items)
+            {
+                planet.Items.Add(item);
+            }
+
+            planets.Add(planet);
+            return $"Successfully added Planet: {planetName}!";
         }
 
         public string ExplorePlanet(string planetName)
         {
-            throw new NotImplementedException();
+            var mission = new Mission();
+            var suitable = astronauts.Models.Where(a => a.Oxygen > 60).ToList();
+            if (suitable.Count == 0)
+            {
+                throw new InvalidOperationException("You need at least one astronaut to explore the planet");
+            }
+
+            mission.Explore(planets.FindByName(planetName), suitable);
+            return $"Planet: {planetName} was explored! Exploration finished with {astronauts.Models.Where(a => a.Oxygen > 0).ToList().Count} dead astronauts!";
         }
 
         public string Report()
@@ -57,7 +76,13 @@ namespace SpaceStation.Core
 
         public string RetireAstronaut(string astronautName)
         {
-            throw new NotImplementedException();
+            if (astronauts.FindByName(astronautName) is null)
+            {
+                throw new InvalidOperationException("Astronaut {astronautName} doesn't exists!");
+            }
+
+            astronauts.Remove(astronauts.FindByName(astronautName));
+            return $"Astronaut {astronautName} was retired!";
         }
     }
 }
