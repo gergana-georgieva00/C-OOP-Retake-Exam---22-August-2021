03f2f857733f5c5da75717d32d1ef77f3e027fca
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-08 22:41:09 +0200

    C# OOP Retake Exam - 22 August 2021

---
+++ SpaceStation/Repositories/PlanetRepository.cs
@@ -0,0 +1,31 @@
+﻿using SpaceStation.Models.Planets.Contracts;
+using SpaceStation.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Repositories
+{
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        private List<IPlanet> models;
+
+        public PlanetRepository()
+        {
+            models = new List<IPlanet>();
+        }
+
+        public IReadOnlyCollection<IPlanet> Models => models.AsReadOnly();
+
+        public void Add(IPlanet model)
+        {
+            models.Add(model);
+        }
+
+        public IPlanet FindByName(string name)
+            => models.Find(m => m.Name == name);
+
+        public bool Remove(IPlanet model)
+            => models.Remove(model);
+    }
+}
