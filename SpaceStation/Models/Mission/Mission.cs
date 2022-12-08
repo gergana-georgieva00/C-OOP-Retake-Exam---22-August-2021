21928f4010a27dc22f5553a6150bef8526105847
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-08 22:33:52 +0200

    C# OOP Retake Exam - 22 August 2021

---
+++ SpaceStation/Models/Mission/Mission.cs
@@ -0,0 +1,25 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission.Contracts;
+using SpaceStation.Models.Planets.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceStation.Models.Mission
+{
+    public class Mission : IMission
+    {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            foreach (var astronaut in astronauts.Where(a => a.Oxygen > 0))
+            {
+                foreach (var item in planet.Items)
+                {
+                    astronaut.Breath();
+                    astronaut.Bag.Items.Add(item);
+                }
+            }
+        }
+    }
+}
---
+++ SpaceStation/Models/Planets/Planet.cs
@@ -0,0 +1,33 @@
+﻿using SpaceStation.Models.Planets.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Planets
+{
+    public class Planet : IPlanet
+    {
+        string name;
+
+        public Planet(string name)
+        {
+            this.Name = name;
+        }
+
+        public ICollection<string> Items { get; private set; }
+
+        public string Name 
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Invalid name!");
+                }
+
+                name = value;
+            }
+        }
+    }
+}
