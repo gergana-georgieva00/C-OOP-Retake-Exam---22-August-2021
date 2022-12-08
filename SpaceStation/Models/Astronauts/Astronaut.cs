48be7d9b100a231facd3cb16c7bb54a4e996a6ad
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-08 12:09:43 +0200

    C# OOP Retake Exam - 22 August 2021

---
+++ SpaceStation/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,57 @@
+﻿using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Bags.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        private string name;
+        private double oxygen;
+
+        public Astronaut(string name, double oxygen)
+        {
+            this.Name = name;
+            this.Oxygen = oxygen;
+        }
+
+        public string Name 
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Astronaut name cannot be null or empty.");
+                }
+
+                name = value;
+            }
+        }
+
+        public double Oxygen 
+        {
+            get => oxygen;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Cannot create Astronaut with negative oxygen!");
+                }
+
+                oxygen = value;
+            }
+        }
+
+        public bool CanBreath { get; private set; }
+
+        public IBag Bag { get; private set; }
+
+        public virtual void Breath()
+        {
+            this.Oxygen -= 10;
+        }
+    }
+}
